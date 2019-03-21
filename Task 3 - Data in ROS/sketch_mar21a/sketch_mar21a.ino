bbf1f4c077ef2bfd21f2e547b2812798b10d43fd
Fox <gregmcintyre1984@gmail.com>
2019-03-21 11:12:08 +1100

    Task 3

---
+++ Task 3 - Data in ROS/sketch_mar21a/sketch_mar21a.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+Zumo32U4ProximitySensors proxSensors;
+//uint16_t lineSensorValues[5] = { 0, 0, 0, 0, 0 };
+bool proxLeftActive;
+bool proxFrontActive;
+bool proxRightActive;
+void setup()
+{
+/* Configuration:
+* - 3 proximity sensors (left, front, right)
+*
+*/
+proxSensors.initThreeSensors();
+}
+
+// Prints a line with all the sensor readings to the serial
+// monitor.
+void printReadingsToSerial()
+{
+static char buffer[80];
+sprintf(buffer, "%d %d %d %d\n",
+proxSensors.countsLeftWithLeftLeds(),
+proxSensors.countsFrontWithLeftLeds(),
+proxSensors.countsFrontWithRightLeds(),
+proxSensors.countsRightWithRightLeds()
+);
+Serial.print(buffer);
+}
+void loop()
+{
+static uint16_t lastSampleTime = 0;
+if ((uint16_t)(millis() - lastSampleTime) >= 100)
+{
+lastSampleTime = millis();
+// Send IR pulses and read the proximity sensors.
+proxSensors.read();
+// Just read the proximity sensors without sending pulses.
+proxLeftActive = proxSensors.readBasicLeft();
+proxFrontActive = proxSensors.readBasicFront();
+proxRightActive = proxSensors.readBasicRight();
+printReadingsToSerial();
+}
+}
---
+++ Task 3 - Data in ROS/sketch_mar21b/sketch_mar21b.ino
@@ -0,0 +1,48 @@
+#define USE_USBCON
+#include <ros.h>
+#include <std_msgs/Int8.h>
+ros::NodeHandle nh;
+std_msgs::Int8 msg;
+ros::Publisher pub("sensor", &msg);
+
+#include <Wire.h>
+#include <Zumo32U4.h>
+Zumo32U4ProximitySensors proxSensors;
+bool proxLeftActive;
+bool proxFrontActive;
+bool proxRightActive;
+void setup()
+{
+/* Configuration:
+* - 3 proximity sensors (left, front, right)
+*
+*/
+proxSensors.initThreeSensors();
+nh.initNode();
+nh.advertise(pub);
+}
+// Prints a line with all the sensor readings to the serial
+// monitor.
+void printReadingsToSerial()
+{
+msg.data = proxSensors.countsLeftWithLeftLeds();
+pub.publish( &msg);
+nh.spinOnce();
+delay(100);
+}
+void loop()
+{
+static uint16_t lastSampleTime = 0;
+if ((uint16_t)(millis() - lastSampleTime) >= 100)
+{
+lastSampleTime = millis();
+// Send IR pulses and read the proximity sensors.
+proxSensors.read();
+// Just read the proximity sensors without sending pulses.
+proxLeftActive = proxSensors.readBasicLeft();
+proxFrontActive = proxSensors.readBasicFront();
+
+proxRightActive = proxSensors.readBasicRight();
+printReadingsToSerial();
+}
+}
