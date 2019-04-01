8e07e917cb78244549cf3ad0920bb9dc833fde46
Fox <gregmcintyre1984@gmail.com>
2019-04-01 14:55:20 +1100

    HD Final

--- Task 4 - Navigation Strategies in ROS/HD/README.md
+++ Task 4 - Navigation Strategies in ROS/HD/README.md
@@ -1,1 +1,5 @@
 HD – Accurate wall avoidance using the Zumo’s full sensor array. Advanced solutions can also consider all IR emitters to each sensor.
+
+Robot reverses when somthing is detected in front, turns right when something is on the left and turns left when something is on the right.
+
+
--- Task 4 - Navigation Strategies in ROS/HD/react.py
+++ Task 4 - Navigation Strategies in ROS/HD/react.py
@@ -9,9 +9,9 @@ pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + ' I heard %d', data.data)
     if data.data > 4:
-        rospy.loginfo(rospy.get_caller_id() + ' I heard %d', data.data)
+        rospy.loginfo(rospy.get_caller_id() + ' I heard %d from the front', data.data)
         vel_msg = Twist()
-        vel_msg.linear.x = -2
+        vel_msg.linear.x = -2       #reverse
         #Since we are moving just in x-axis
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
@@ -20,12 +20,42 @@ def callback(data):
         vel_msg.angular.z = 0
         pub.publish(vel_msg)
 
+def callleft(data):
+    #rospy.loginfo(rospy.get_caller_id() + ' I heard %d', data.data)
+    if data.data > 4:
+        rospy.loginfo(rospy.get_caller_id() + ' I heard %d from the left', data.data)
+        vel_msg = Twist()
+        vel_msg.linear.x = 0
+        #Since we are moving just in x-axis
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = -2
+        pub.publish(vel_msg)
+
+def callright(data):
+    #rospy.loginfo(rospy.get_caller_id() + ' I heard %d', data.data)
+    if data.data > 4:
+        rospy.loginfo(rospy.get_caller_id() + ' I heard %d from the right', data.data)
+        vel_msg = Twist()
+        vel_msg.linear.x = 0
+        #Since we are moving just in x-axis
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 2
+        pub.publish(vel_msg)
+
+
+
 def listener():
     #rospy.init_node('listener', anonymous=True)
     #rospy.Subscriber('sensor', Int8, callback)
-    rospy.Subscriber('proxLeftSensor', Int8, callback)
-    #rospy.Subscriber('proxFrontSensor', Int8, callback)    #not required for task
-    rospy.Subscriber('proxRightSensor', Int8, callback)
+    rospy.Subscriber('proxLeftSensor', Int8, callleft)
+    rospy.Subscriber('proxFrontSensor', Int8, callback)
+    rospy.Subscriber('proxRightSensor', Int8, callright)
 
     #spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
